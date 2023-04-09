0d6e405a46e2a365c9810b8876a25d39993296a4
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-04-09 12:37:48 +0530

    adding functionality of signup, placed order

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,3 +1,29 @@
+// import express from 'express';
+// import Product from '../models/productModel.js';
+// import data from '../data.js';
+
+// const seedRouter = express.Router();
+
+// seedRouter.get('/', async (req, res) => {
+//   await Product.remove({});
+//   const createdProducts = await Product.insertMany(data.products);
+//   res.send({ createdProducts });
+// });
+// export default seedRouter;
+
+// import express from 'express';
+// import Product from '../models/productModel.js';
+// import data from '../data.js';
+
+// const seedRouter = express.Router();
+
+// seedRouter.get('/', async (req, res) => {
+//   // await Product.remove({});
+//   const createdProducts = await Product.insertMany(data.products);
+//   res.send({ createdProducts });
+// });
+// export default seedRouter;
+
 import express from 'express';
 import Product from '../models/productModel.js';
 import data from '../data.js';
@@ -6,9 +32,9 @@ import User from '../models/userModel.js';
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.remove({});
+  await Product.deleteMany({});
   const createdProducts = await Product.insertMany(data.products);
-  await User.remove({});
+  await User.deleteMany({});
   const createdUsers = await User.insertMany(data.users);
   res.send({ createdProducts, createdUsers });
 });
--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,17 @@ import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('connected to db');
   })
   .catch((err) => {
     console.log(err.message);
-    console.log(`error check`);
   });
 
 const app = express();
@@ -28,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
